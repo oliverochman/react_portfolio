03546a5459677e73a567a29e8235a7fd5a54133f
oliverochman <oliver.ochman@hotmail.com>
2019-11-29 10:39:04 +0100

    add projects.json

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,49 @@
-import React from "react"
+import React, { Component } from "react"
+import axios from "axios"
 
-const Projects = () => {
-  return (
-    <div className="ui container">
-      <h1 className="ui header">My Projects</h1>
-      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-    </div>
-  )
+class Projects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    };
+  }
 
+  componentDidMount() {
+    axios.get('./src/data/projects.json')
+      .then(response => {
+        this.setState({
+          projects: response.data
+        })
+      })
+  }
+
+  render() {
+    const projects = this.state.projects
+    let projectsList
+
+    if (projects.length > 0) {
+      projectsList = projects.map(project => {
+        return (
+          <div key={project.id}>
+            <h3 className="ui header">
+              {project.name}
+            </h3>
+          </div>
+        )
+      })
+    }
+
+
+    return (
+
+      <div className="ui main container">
+        <h1 className="ui header">My Projects</h1>
+        {projectsList}
+      </div>
+
+    )
+  }
 };
 
 export default Projects
